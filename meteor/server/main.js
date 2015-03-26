d5b060afdc6bc0dc04a0f67ba8ffd30da804de76
Aranir <rogerkueng@gmail.com>
2015-03-26 15:57:48 +0100

    Add methods to fetch data and gameboard

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -10,25 +10,43 @@ Meteor.startup(function () {
     //    Meteor.users.insert(user1)
     //}
 
-    Accounts.createUser({
-        username: "mino",
-        email : "minos@mai.com",
-        password : "something",
-        profile  : {
-            //publicly visible fields like firstname goes here
-            firstname:
-        }
 
-    });
 
-    if (Gameboards.find().count() == 0){
-        console.log("We have no gameboards");
-    }
-    console.log(Meteor.users)
+    //if (Gameboards.find().count() == 0){
+    //    console.log("We have no gameboards");
+    //}
 });
 
 Meteor.methods({
     sayHello: function(text){
         console.log("hello " + text);
+    },
+    fetchGameBoard: function(){
+        var userId = "10153179507419968";
+        var gameCreator = process.env.GAMECREATOR_URL;
+        var url = gameCreator+ "/gameboard?user_id=" + userId;
+        console.log(url);
+
+        Meteor.http.get(url, function(err, res){
+            if (err){
+                console.log("Fetching Gameboard for user " + userId + " went wrong " + err)
+            } else {
+                Gameboards.insert(res.data);
+            }
+
+        });
+    },
+    fetchData: function(){
+        var accesstoken = "CAACEdEose0cBAM810upRgDgZAkh5fUm9iNknOhbWfGfJrsYAFowKR6oPTomH87s7kYn7pnVcNOu2iVudoaVXhO3wVDfjEGetjeA1TKVsQkxOGhLtWY6oi9QwnAo11DddNkABttO4NeDFLknlTxKZC7HaXDPeRHg1ZBGeAeiRWLcJ80WbLRkY0Y2RRRq82ISR3rqEl8r7MEEsG7VmEMBgwJoou6YV94ZD";
+        var userId = "10153179507419968";
+        var gameCreator = process.env.GAMECREATOR_URL;
+
+        var url = gameCreator + "/fetchData?user_id=" + userId + "&access_token=" + accesstoken;
+        console.log(url);
+        Meteor.http.get(url, function (err, res) {
+            console.log(res.statusCode, res.data);
+        });
     }
+
+
 });
