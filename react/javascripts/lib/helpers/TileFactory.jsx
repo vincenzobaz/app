2538400bdd50c56ac47c5a38a3418fc86719d7e0
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-05-04 02:12:31 +0200

    Refactor TileFactory a little bit.

--- react/javascripts/lib/helpers/TileFactory.jsx
+++ react/javascripts/lib/helpers/TileFactory.jsx
@@ -1,50 +1,34 @@
 
 'use strict';
 
-var React = require('react'),
-    Tile = require('../components/Tile'),
-    debug = require('debug')('TileFactory');
+const React = require('react'),
+      Tile = require('../components/Tile'),
+      debug = require('debug')('TileFactory');
 
 class TileFactory {
 
   constructor(game, modalFactory) {
-    this.curTileNum = 1;
     this.game = game;
     this.board = game.getBoard();
-    console.log(window.board = this.board);
     this.modalFactory = modalFactory;
   }
 
-  // TODO: Replace this.curTileNum with a fold.
-  // TODO: Refactor makeTiles().
   getTiles(tiles) {
-    this.curTileNum = 1;
-    return this.board.getTiles().map(this.makeTile.bind(this));
+    return this.board.getTiles().map(
+      (tile, n) => this.makeTile(tile, n + 1)
+    );
   }
 
-  makeTile(tile) {
-    var modal     = this.modalFactory.makeModal(tile);
-    var tileNum   = this.curTileNum;
-    var placement = this.getPlacementForTileAt(tileNum);
+  makeTile(tile, tileNum) {
+    const modal      = this.modalFactory.makeModal(tile);
+    const placement  = this.placeTileAt(tileNum);
+    const opponentId = null;
 
-    this.curTileNum += 1;
+    const icon       = tile.getIcon();
+    const category   = tile.getCategory();
 
-    var opponentId;
-
-    var icon = tile.getIcon();
-    var category = tile.getCategory();
-
-    if (tileNum === 5) {
-      var op = this.game.getOpponent();
-
-      opponentId = op.getFacebookId();
-      icon = 'opponent';
-      category = `Questions about ${op.getFirstName()}`;
-    }
-
-    var type     = icon;
-    var answered = (tile.getScore().them >= 3) ? true : tile.isAnswered();
-    var wonBy    = this.computeWin(tileNum - 1);
+    const type       = icon;
+    const answered   = (tile.getScore().them >= 3) ? true : tile.isAnswered();
 
     return (
       <Tile key={'tile-' + tileNum}
@@ -57,32 +41,13 @@ class TileFactory {
             score={tile.getScore()}
             questionModal={modal}
             opponentId={opponentId}
-            wonBy={wonBy}
             disabled={this.game.hasEnded()} />
     );
   }
 
-  getPlacementForTileAt(n) {
-    var p = ['left', 'top', 'right'];
+  placeTileAt(n) {
+    const p = ['left', 'top', 'right'];
     return p[(n - 1) % p.length];
-  }
-
-  computeWin(tileNum) {
-    if (!this.game.isWon()) {
-      return null;
-    }
-
-    var winData = this.game.getWinData();
-    var winningMoves = JSON.parse(winData.winningMove);
-    var won = winningMoves.indexOf(tileNum) > -1;
-
-    if (!won) {
-      return null;
-    }
-
-    return (winData.wonBy !== this.game.getOpponent().getFacebookId()) ?
-      'me' :
-      'opponent';
   }
 
 }
