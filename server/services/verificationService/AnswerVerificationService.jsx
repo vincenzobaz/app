f122363794618ddc40fbf0c7309529f4c40f230d
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-10-15 16:57:04 +0200

    Refactor AnswerVerificationService.

--- server/services/verificationService/AnswerVerificationService.jsx
+++ server/services/verificationService/AnswerVerificationService.jsx
@@ -1,3 +1,4 @@
+
 class Answer {
     /**
      * The time spend to answer the question in ms
@@ -12,8 +13,29 @@ class Answer {
     data;
 }
 
-
 AnswerVerificationService = class AnswerVerificationService {
+
+    static getVerifiers() {
+      const verifiers = {};
+
+      verifiers[Question.Kind.Timeline]       = TimelineVerificationService;
+      verifiers[Question.Kind.MultipleChoice] = MultipleChoiceVerificationService;
+      verifiers[Question.Kind.Geolocation]    = GeoVerificationService;
+      verifiers[Question.Kind.Order]          = OrderVerificationService;
+
+      return verifiers;
+    }
+
+    static getVerifier(kind) {
+      const verifiers = AnswerVerificationService.getVerifiers();
+      const verifier  = verifiers[kind];
+
+      if (!AnswerVerificationService.isVerifier(verifier)) {
+        return null;
+      }
+
+      return verifier;
+    }
 
     /**
      * @param {Tile} tile
@@ -22,23 +44,27 @@ AnswerVerificationService = class AnswerVerificationService {
      * @return {[number]} the array of the results: 0 for incorrect 1 for correct per question
      */
     static verifyTile(tile, answers) {
-
         const questionAnswers = _.zip(tile.getQuestions(), answers);
-        console.log("ansswers", answers);
+
         return _.map(questionAnswers, qa => {
-            if (qa[0].getKind() === Question.Kind.Timeline){
-                return TimelineVerificationService.verifyAnswer(qa[0], qa[1]);
-            } else if (qa[0].getKind() === Question.Kind.MultipleChoice){
-                return MultipleChoiceVerificationService.verifyAnswer(qa[0], qa[1]);
-            } else if (qa[0].getKind() === Question.Kind.Geolocation) {
-                console.error("verifying geo", qa[1]);
-                return GeoVerificationService.verifyAnswer(qa[0], qa[1]);
-            } else if (qa[0].getKind() === Question.Kind.Order) {
-                return OrderVerificationService.verifyAnswer(qa[0], qa[1]);
-            } else {
-                console.error("got invalid question kind " + qa[0].getKind());
-                return 0;
-            }
+          const kind     = qa[0].getKind();
+          const verifier = AnswerVerificationService.getVerifier(kind);
+
+          if (verifier == null) {
+            console.error(`AnswerVerificationService: Got invalid question kind: ${kind}`);
+            return 0;
+          }
+
+          return verifier.verifyAnswer(qa[0], qa[1]);
         });
     }
+
+    static isVerifier(verifier) {
+      return (
+        verifier != null &&
+        typeof verifier === 'object' &&
+        typeof verifier.verifyAnswer === 'function'
+      );
+    }
 };
+
