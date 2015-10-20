1e5fd9062ec6dd1a5f6f61b58dcb64117c7485e5
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-10-20 15:19:58 +0200

    Less logging.

--- server/publish.jsx
+++ server/publish.jsx
@@ -1,6 +1,8 @@
 
+const LOG_PUBLISH = false;
+
 Meteor.publish('games', function() {
-    console.log(`Publishing games for user ${this.userId}...`);
+    LOG_PUBLISH && console.log(`Publishing games for user ${this.userId}...`);
 
     return Games.find({
         $or: [
@@ -11,31 +13,32 @@ Meteor.publish('games', function() {
 });
 
 Meteor.publish('gameBoards', function() {
-    console.log(`Publishing game boards for user ${this.userId}...`);
+    LOG_PUBLISH && console.log(`Publishing game boards for user ${this.userId}...`);
 
     return GameBoards.find({ userId: this.userId });
 });
 
 Meteor.publish('joinRequests', function() {
-    console.log(`Publishing join requests for user ${this.userId}...`);
+    LOG_PUBLISH && console.log(`Publishing join requests for user ${this.userId}...`);
 
     return JoinRequests.find({ to: this.userId });
 });
 
 // TODO: Don't publish access token etc.
 Meteor.publish('userServices', function() {
-    console.log(`Publishing services for user ${this.userId}...`);
+    LOG_PUBLISH && console.log(`Publishing services for user ${this.userId}...`);
 
     return Meteor.users.find({ _id: this.userId }, { fields: { 'services': 1 } });
 });
 
 Meteor.publish('friends', function() {
-    console.log(`Publishing friends for user ${this.userId}...`);
+    LOG_PUBLISH && console.log(`Publishing friends for user ${this.userId}...`);
 
     return Friends.find({ friendOf: this.userId });
 });
 
 Meteor.publish('gameStats', function() {
+    LOG_PUBLISH && console.log(`Publishing game stats for user ${this.userId}...`);
     return Gamestats.find();
 });
 
