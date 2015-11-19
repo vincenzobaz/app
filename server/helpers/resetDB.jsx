0c285c38142d2a51c2532ce312de9714c745b4e8
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-11-19 17:51:08 +0100

    Add option to reset all db.

--- server/helpers/resetDB.jsx
+++ server/helpers/resetDB.jsx
@@ -13,13 +13,15 @@ const defaults = {
 resetDB = (override = {}) => {
     const toDelete = _.extend({}, defaults, override);
     const collections = [];
-    if (toDelete.Games)         collections.push(Games);
-    if (toDelete.JoinRequests)  collections.push(JoinRequests);
-    if (toDelete.GameBoards)    collections.push(GameBoards);
-    if (toDelete.GameFetches)   collections.push(GameFetches);
-    if (toDelete.Answers)       collections.push(Answers);
-    if (toDelete.Users)         collections.push(Meteor.users);
-    if (toDelete.Friends)       collections.push(Friends);
+    const all = override.All;
+
+    if (all || toDelete.Games)         collections.push(Games);
+    if (all || toDelete.JoinRequests)  collections.push(JoinRequests);
+    if (all || toDelete.GameBoards)    collections.push(GameBoards);
+    if (all || toDelete.GameFetches)   collections.push(GameFetches);
+    if (all || toDelete.Answers)       collections.push(Answers);
+    if (all || toDelete.Users)         collections.push(Meteor.users);
+    if (all || toDelete.Friends)       collections.push(Friends);
 
     collections.forEach(coll => coll.remove({}));
 };
