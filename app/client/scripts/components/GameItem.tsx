18f2c801e0d7aea24a43f8df9c3258a287bdd0a2
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-04-06 16:22:36 +0200

    Show if game was won/draw/lost in past games list.

--- app/client/scripts/components/GameItem.tsx
+++ app/client/scripts/components/GameItem.tsx
@@ -1,4 +1,3 @@
-'use strict';
 
 import {GameStore} from './../stores/GameStore';
 import {GameStatus, GAME_STATUS} from './../../../common/models/GameStatus';
@@ -6,20 +5,18 @@ import {Game} from "../models/Game";
 import {Friend} from "../../../common/models/Friend";
 import {Score} from "../../../common/models/Score";
 
-
 interface GameItemProps {
   game: Game;
 }
 
 export class GameItem extends React.Component<GameItemProps, {}> {
 
-
   render() {
-    const game: Game = this.props.game;
+    const game: Game       = this.props.game;
     const opponent: Friend = game.opponent as Friend;
-    const opponentName = opponent != null ? opponent.name : 'Loading...';
-    const avatarUrl = opponent != null ? opponent.avatarUrl : '';
-    const classNames = this.getClassNames();
+    const opponentName     = opponent != null ? opponent.name      : 'Loading...';
+    const avatarUrl        = opponent != null ? opponent.avatarUrl : '';
+    const classNames       = this.getClassNames();
 
     return (
         <li className={classNames.waiting}>
@@ -41,35 +38,8 @@ export class GameItem extends React.Component<GameItemProps, {}> {
   }
 
   renderDescription() {
-    var game = this.props.game;
-    var desc = <small></small>;
-
-    switch (game.status) {
-      case GAME_STATUS.Creating:
-        return <small><b>In creation</b></small>;
-
-      case GAME_STATUS.Waiting:
-        return <small><b>Waiting</b></small>;
-
-      case GAME_STATUS.Declined:
-        return <small><b>Declined</b></small>;
-
-      case GAME_STATUS.Failed:
-        return <small><b>Failed</b></small>;
-
-      case GAME_STATUS.Ended:
-        desc = <small><b>Ended</b></small>;
-        break;
-
-      case GAME_STATUS.Playing:
-        if (game.isMyTurnToPlay) {
-          desc = <small><b className='player'>Your turn</b></small>;
-        } else {
-          desc = <small><b>Their turn</b></small>;
-        }
-    }
-
-    var score:Score = this.props.game.score;
+    const game: Game   = this.props.game;
+    const score: Score = this.props.game.score;
 
     return (
         <small>
@@ -77,11 +47,45 @@ export class GameItem extends React.Component<GameItemProps, {}> {
           –
           <b>{score.them}</b>
           &nbsp;
-          ({desc})
+          (<small>{this.renderGameStatus(game)}</small>)
         </small>
     );
   }
-  
+
+  renderGameStatus(game: Game) {
+    switch (game.status) {
+      case GAME_STATUS.Creating:
+        return <b>In creation</b>;
+
+      case GAME_STATUS.Waiting:
+        return <b>Waiting</b>;
+
+      case GAME_STATUS.Declined:
+        return <b>Declined</b>;
+
+      case GAME_STATUS.Failed:
+        return <b>Failed</b>;
+
+      case GAME_STATUS.Ended:
+        var text = '';
+
+        if (game.isWon)       text = 'Won';
+        else if (game.isDraw) text = 'Draw';
+        else                  text = 'Lost';
+
+        return <b className={game.isWon && 'player'}>{text}</b>;
+
+      case GAME_STATUS.Playing:
+        if (game.isMyTurnToPlay) {
+          return <b className='player'>Your turn</b>;
+        }
+
+        return <b>Their turn</b>;
+
+      default:
+        return <b>?</b>;
+    }
+  }
 
   switchToGame(game) {
     return (e) => {
