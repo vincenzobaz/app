95ec42e34637face34e75e1c9eadf1bc6fe6f569
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-07-09 19:23:34 +0200

    Cleanup main.tsx.

--- app/client/main.tsx
+++ app/client/main.tsx
@@ -1,80 +1,59 @@
 import * as React     from 'react';
 import * as ReactDOM  from 'react-dom';
 
-import {ErrorStore}        from './stores/ErrorStore';
-import {ModalStore}        from './stores/ModalStore';
-import {NotificationStore} from './stores/NotificationStore';
-import {ErrorHandler}      from './components/ErrorHandler';
-import {ModalHandler}      from './components/ModalHandler';
-import {Main}              from './handlers/Main';
-import {FacebookClientService} from "./services/FacebookClientService";
-import {Feedback, FEEDBACK_STATUS} from "../common/models/Feedback";
-import {StateCollector} from "./StateCollector";
+import {ErrorStore}                from './stores/ErrorStore';
+import {ModalStore}                from './stores/ModalStore';
+import {NotificationStore}         from './stores/NotificationStore';
+import {ErrorHandler}              from './components/ErrorHandler';
+import {ModalHandler}              from './components/ModalHandler';
+import {Main}                      from './handlers/Main';
+import {FacebookClientService}     from './services/FacebookClientService';
+import {Feedback, FEEDBACK_STATUS} from '../common/models/Feedback';
+import {StateCollector}            from './StateCollector';
 
 const $$ = document.getElementById.bind(document);
 
 class App {
 
-  boot():void {
-    ErrorStore.register();
+  boot(): void {
     this.subscribe();
-    $['feedback']({
-      ajaxURL: 'http://test.url.com/feedback',
-      html2canvasURL: 'js/html2canvas.js',
-      onFeedbackSend: (feedback:Feedback, onSuccess:Function, onFailure:Function) => {
-        console.log(feedback);
-        feedback.userId = Meteor.userId();
-        let {game, tile, question} = StateCollector.getState();
-        feedback.game = game;
-        feedback.tile = tile;
-        feedback.question = question;
-        feedback.creationDate = new Date();
-        feedback.status = FEEDBACK_STATUS.New;
-        Meteor.call('SendFeedback', feedback, (error:Meteor.Error, success) => {
-          if (!error) {
-            onSuccess();
-          } else {
-            onFailure();
-          }
 
-        })
-      }
-    });
     if (this.isLoggedIn()) {
       this.enableNotifications();
       this.loadFacebookSdk();
+      this.enableFeedback();
     }
   }
 
-    render():void {
-        ReactDOM.render(<ErrorHandler store={ErrorStore}/>, $$('error'));
-        ReactDOM.render(<ModalHandler store={ModalStore}/>, $$('modal'));
-        ReactDOM.render(<Main />, $$('app'));
-    }
+  render():void {
+      ReactDOM.render(<ErrorHandler store={ErrorStore.getInstance()}/>, $$('error'));
+      ReactDOM.render(<ModalHandler store={ModalStore}/>, $$('modal'));
+      ReactDOM.render(<Main />, $$('app'));
+  }
 
-    isLoggedIn():boolean {
-        return Meteor.userId() != null;
-    }
+  isLoggedIn():boolean {
+      return Meteor.userId() != null;
+  }
 
-    subscribe():void {
-        console.log('Subscribing to Meteor channels...');
+  subscribe():void {
+      console.log('Subscribing to Meteor channels...');
 
-        Meteor.subscribe('games');
-        Meteor.subscribe('gameBoards');
-        Meteor.subscribe('joinRequests');
-        Meteor.subscribe('userServices');
-        Meteor.subscribe('friends');
-        Meteor.subscribe('notifications');
-        Meteor.subscribe('feedback');
-    }
+      Meteor.subscribe('games');
+      Meteor.subscribe('gameBoards');
+      Meteor.subscribe('joinRequests');
+      Meteor.subscribe('userServices');
+      Meteor.subscribe('friends');
+      Meteor.subscribe('notifications');
+      Meteor.subscribe('feedback');
+  }
 
-    enableNotifications():void {
-        NotificationStore.requestPermissionIfNeeded();
+  enableNotifications():void {
+      NotificationStore.requestPermissionIfNeeded();
 
-        Tracker.autorun(() => {
-            NotificationStore.fetchAndShow();
-        });
-    }
+      Tracker.autorun(() => {
+          NotificationStore.fetchAndShow();
+      });
+  }
 
   loadFacebookSdk(count: number = 0): void {
     const fbConfig = ServiceConfiguration.configurations.findOne({
@@ -111,6 +90,30 @@ class App {
      }(document, 'script', 'facebook-jssdk'));
   }
 
+  enableFeedback() {
+    $['feedback']({
+      ajaxURL: 'http://test.url.com/feedback',
+      html2canvasURL: 'js/html2canvas.js',
+      onFeedbackSend: (feedback: Feedback, onSuccess: Function, onFailure: Function) => {
+        const { game, tile, question } = StateCollector.getState();
+
+        feedback.userId            = Meteor.userId();
+        feedback.game              = game;
+        feedback.tile              = tile;
+        feedback.question          = question;
+        feedback.creationDate      = new Date();
+        feedback.status            = FEEDBACK_STATUS.New;
+
+        Meteor.call('SendFeedback', feedback, (error, success) => {
+          if (!error) {
+            onSuccess();
+          } else {
+            onFailure();
+          }
+        });
+      }
+    });
+  }
 
 }
 
