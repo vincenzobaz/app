69b467644d9e5dcd0eaedcb716a199ff6e89ee78
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-07-17 20:58:31 +0200

    Prevent router from changing.

--- app/client/handlers/Main.tsx
+++ app/client/handlers/Main.tsx
@@ -9,22 +9,22 @@ import {Home}       from './Home';
 import {Dashboard} from './Dashboard';
 import {BugBoard} from "../components/BugBoard";
 
+const router = (
+  <Router history={browserHistory}>
+      <Route path="/" component={Dashboard}>
+          <IndexRoute component={Welcome}/>
+
+          <Route path="about" component={About}/>
+          <Route path="account" component={Account}/>
+          <Route path="play/:gameId" component={PlayGame}/>
+      </Route>
+      <Route path="bugboard" component={BugBoard}/>
+  </Router>
+);
+
 export class Main extends React.Component<{}, {}> {
-
     render() {
-        return (
-            <Router history={browserHistory}>
-                <Route path="/" component={Dashboard}>
-                    <IndexRoute component={Welcome}/>
-
-                    <Route path="about" component={About}/>
-                    <Route path="account" component={Account}/>
-                    <Route path="play/:gameId" component={PlayGame}/>
-                </Route>
-                <Route path="bugboard" component={BugBoard}/>
-            </Router>
-        );
+      return router;
     }
-
 }
 
