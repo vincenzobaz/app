66c3863496b217be58ae5c4bc99f285741e62bf7
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-11-02 16:58:05 +0100

    Implement new questions using facebook reactions

--- meteor/imports/common/models/questions/common/QuestionType.ts
+++ meteor/imports/common/models/questions/common/QuestionType.ts
@@ -1,6 +1,13 @@
-export type QuestionType = "MCWhoLikedYourPost" |
-    "MCWhoMadeThisCommentOnYourPost" |
+export type QuestionType = "MCWhoMadeThisCommentOnYourPost" |
     "MCWhichPageDidYouLike" |
+    "MCWhoReactedToYourPost" |
+    "MCWhoReactedToYourPostWithLIKE" |
+    "MCWhoReactedToYourPostWithWOW" |
+    "MCWhoReactedToYourPostWithHAHA" |
+    "MCWhoReactedToYourPostWithLOVE" |
+    "MCWhoReactedToYourPostWithSAD" |
+    "MCWhoReactedToYourPostWithANGRY" |
+    "MCWhoReactedToYourPostWithTHANKFUL" |
     "TLWhenDidYouShareThisPost" |
     "TLWhenDidYouLikeThisPage" |
     "GeoWhatCoordinatesWereYouAt" |
@@ -13,9 +20,16 @@ export type QuestionType = "MCWhoLikedYourPost" |
 
 
 export const QUESTION_TYPE = {
-  MCWhoLikedYourPost: 'MCWhoLikedYourPost' as QuestionType,
   MCWhoMadeThisCommentOnYourPost: 'MCWhoMadeThisCommentOnYourPost' as QuestionType,
   MCWhichPageDidYouLike: 'MCWhichPageDidYouLike' as QuestionType,
+  MCWhoReactedToYourPost: 'MCWhoReactedToYourPost' as QuestionType,
+  MCWhoReactedToYourPostWithLIKE: 'MCWhoReactedToYourPostWithLIKE' as QuestionType,
+  MCWhoReactedToYourPostWithWOW: 'MCWhoReactedToYourPostWithWOW' as QuestionType,
+  MCWhoReactedToYourPostWithHAHA: 'MCWhoReactedToYourPostWithHAHA' as QuestionType,
+  MCWhoReactedToYourPostWithLOVE: 'MCWhoReactedToYourPostWithLOVE' as QuestionType,
+  MCWhoReactedToYourPostWithSAD: 'MCWhoReactedToYourPostWithSAD' as QuestionType,
+  MCWhoReactedToYourPostWithANGRY: 'MCWhoReactedToYourPostWithANGRY' as QuestionType,
+  MCWhoReactedToYourPostWithTHANKFUL: 'MCWhoReactedToYourPostWithTHANKFUL' as QuestionType,
   TLWhenDidYouShareThisPost: 'TLWhenDidYouShareThisPost' as QuestionType,
   TLWhenDidYouLikeThisPage: 'TLWhenDidYouLikeThisPage' as QuestionType,
   GeoWhatCoordinatesWereYouAt: 'GeoWhatCoordinatesWereYouAt' as QuestionType,
