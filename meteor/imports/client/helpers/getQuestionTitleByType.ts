8ad1a5a3a9d4c3f50138df15384cbd7f75c8b1b1
Jeremy Rabasco <rabasco.jeremy@gmail.com>
2016-11-21 18:56:27 +0100

    Change question text

--- meteor/imports/client/helpers/getQuestionTitleByType.ts
+++ meteor/imports/client/helpers/getQuestionTitleByType.ts
@@ -10,7 +10,7 @@ const en = {
   ORDPostCommentsNumber: 'Order these posts according to the number of comments they got: least comments first',
   ORDPostLikesNumber: 'Order these posts according to the number of likes: fewest likes on top',
   ORDPostTime: 'Order theses posts after the time you posted them: oldest on top',
-  MCWhoReactedToYourPost: 'Who reacted (Like, Wow, Haha, Love, Sad, Angry, Thankful) to this post you made?',
+  MCWhoReactedToYourPost: 'Who reacted (Comment, Like, Wow, Haha, Love, Sad, Angry, Thankful) to this post you made?',
   MCWhoReactedToYourPostWithLIKE: 'Who liked this post you made?',
   MCWhoReactedToYourPostWithWOW: 'Who was impressed by this post you made?',
   MCWhoReactedToYourPostWithHAHA: 'Who laughed at this post you made?',
