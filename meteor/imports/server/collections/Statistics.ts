fcd170999105be26a8ec91d93ef858a2f57db822
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-11-30 19:27:20 +0100

    Rename collection

--- meteor/imports/server/collections/Statistics.ts
+++ meteor/imports/server/collections/Statistics.ts
@@ -4,7 +4,7 @@ import {RawStats, Stats} from "../../common/models/Stats"
  * Collection used for caching user statistics
  * @type {Mongo.Collection<Stats>}
  */
-export const Statistics = new Mongo.Collection<Stats>('stats', {
+export const Statistics = new Mongo.Collection<Stats>('statistics', {
     transform(doc: RawStats) {
         return Stats.fromRaw(doc);
     }
