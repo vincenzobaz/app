ed47f8f44f30dd92a8ceff7f4a2697f9830e1d62
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-12-01 15:17:02 +0100

    Test without types

--- meteor/imports/client/collections/Statistics.ts
+++ meteor/imports/client/collections/Statistics.ts
@@ -2,6 +2,7 @@
 import {Stats, RawStats} from "../../common/models/Stats";
 
 export const Statistics = new Mongo.Collection<Stats>('statistics', {
+
     transform(doc: RawStats) {
         return Stats.fromRaw(doc);
     }
