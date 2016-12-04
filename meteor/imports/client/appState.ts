441dc3ed50d43de92514bff31a76eb4b15b8fd96
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-12-04 18:13:44 +0100

    Access stats through app state

--- meteor/imports/client/appState.ts
+++ meteor/imports/client/appState.ts
@@ -1,33 +1,32 @@
+import {UserStore}        from './stores/UserStore';
+import {GameStore}        from './stores/GameStore';
+import {FriendStore}      from './stores/FriendStore';
+import {JoinRequestStore} from './stores/JoinRequestStore';
 
-import { UserStore }        from './stores/UserStore';
-import { GameStore }        from './stores/GameStore';
-import { FriendStore }      from './stores/FriendStore';
-import { JoinRequestStore } from './stores/JoinRequestStore';
-
-import { User }             from './models/User';
-import { Game }             from './models/Game';
-import { Friend }           from '../common/models/Friend';
-import { JoinRequest }      from './models/JoinRequest';
+import {User}             from './models/User';
+import {Game}             from './models/Game';
+import {Friend}           from '../common/models/Friend';
+import {JoinRequest}      from './models/JoinRequest';
 import {Statistics} from "./collections/Statistics";
 import {Stats} from "../common/models/Stats";
 
 export interface AppState {
-  isLoggedIn   : boolean;
-  user         : User;
-  games        : Game[];
-  friends      : Friend[];
-  joinRequests : JoinRequest[];
-  stats : Stats[];
+    isLoggedIn: boolean;
+    user: User;
+    games: Game[];
+    friends: Friend[];
+    joinRequests: JoinRequest[];
+    stats: Stats[];
 }
 
 export function getAppState(): AppState {
-  return {
-    isLoggedIn   : UserStore.isLoggedIn()  || false,
-    user         : UserStore.current()     || null,
-    games        : GameStore.list()        || [],
-    friends      : FriendStore.list()      || [],
-    joinRequests : JoinRequestStore.list() || [],
-      stats : Statistics.find().fetch()
-  };
+    return {
+        isLoggedIn: UserStore.isLoggedIn() || false,
+        user: UserStore.current() || null,
+        games: GameStore.list() || [],
+        friends: FriendStore.list() || [],
+        joinRequests: JoinRequestStore.list() || [],
+        stats: Statistics.find().fetch()
+    };
 }
 
