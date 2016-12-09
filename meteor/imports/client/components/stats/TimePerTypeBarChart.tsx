b4434246cbb39f74aa163e841f468fe17bcadbc0
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-12-09 16:17:57 +0100

    Fix statistics error

--- meteor/imports/client/components/stats/TimePerTypeBarChart.tsx
+++ meteor/imports/client/components/stats/TimePerTypeBarChart.tsx
@@ -4,32 +4,39 @@ const Bar = require('react-chartjs-2').Bar;
 
 
 export function TimePerTypeBarChart(props) {
-   const stats: Stats[] = props.stats;
+    const stats: Stats[] = props.stats;
 
-   let timePerType: {[key: string]: number} = {};
-   Kinds.forEach(kind => timePerType[kind] = 0);
+    let timePerType: {[key: string]: number} = {};
+    let amountPerType: {[key: string]: number} = {};
 
-   for (let stat of stats) {
-       for (let kind of Kinds) {
-           timePerType[kind] += stat.counters[kind].timeSpent;
-       }
-   }
+    Kinds.forEach(kind => {
+        timePerType[kind] = 0;
+        amountPerType[kind] = 0;
+    });
 
-   let averages: number[] = Kinds.map(kind => timePerType[kind] / stats.length);
+    for (let stat of stats) {
+        for (let kind of Kinds) {
+            timePerType[kind] += stat.counters[kind].timeSpent;
+            amountPerType[kind] += stat.counters[kind].amount;
+        }
+    }
 
-   let data = {
-       labels: Kinds,
-       datasets: [{
-           label: 'Average time (milliseconds)',
-           data: averages,
-           backgroundColor: 'rgba(255, 165, 0, 0.8)'
-       }]
-   };
-   let options = {
-       title: 'Average time spent per question type'
-   };
+    let averages: number[] =
+        Kinds.map(kind => timePerType[kind] / amountPerType[kind]);
 
-   return (
-       <Bar data={data} options={options}/>
-   );
+    let data = {
+        labels: Kinds,
+        datasets: [{
+            label: 'Average time (milliseconds)',
+            data: averages,
+            backgroundColor: 'rgba(255, 165, 0, 0.8)'
+        }]
+    };
+    let options = {
+        title: 'Average time spent per question type'
+    };
+
+    return (
+        <Bar data={data} options={options}/>
+    );
 }
