c0ac1f4fb2b0eb9344ddfa42c5fe5bcedeca870a
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-12-12 13:50:19 +0100

    Add button

--- meteor/imports/client/components/AccountSettings.tsx
+++ meteor/imports/client/components/AccountSettings.tsx
@@ -1,32 +1,32 @@
-
 import {
-  Button,
-  OverlayTrigger,
-  Popover,
-  Row, Col, Well
+    Button,
+    OverlayTrigger,
+    Popover,
+    Row, Col, Well
 } from 'react-bootstrap';
+import {getConfig} from "../helpers/getConfig";
 
 interface AccountSettingsState {
-    logoutConfirmed:boolean;
-    deleteAllDataConfirmed:boolean;
+    logoutConfirmed: boolean;
+    deleteAllDataConfirmed: boolean;
 }
 
 export class AccountSettings extends React.Component<{}, AccountSettingsState> {
 
     constructor(props) {
-      super(props);
+        super(props);
 
-      this.state = {
-        logoutConfirmed: false,
-        deleteAllDataConfirmed: false
-      }
+        this.state = {
+            logoutConfirmed: false,
+            deleteAllDataConfirmed: false
+        }
     }
 
     render() {
         let popover = (
-          <Popover id="logoutInfo">
-            Are you sure? Click again to confirm
-          </Popover>
+            <Popover id="logoutInfo">
+                Are you sure? Click again to confirm
+            </Popover>
         );
 
         return (
@@ -46,9 +46,9 @@ export class AccountSettings extends React.Component<{}, AccountSettingsState> {
                     <Row>
                         <Col md={6} mdOffset={3}>
                             <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={popover}>
-                              <Button className="button-logout" onClick={this.onLogout.bind(this)}>
-                                Log Out
-                              </Button>
+                                <Button className="button-logout" onClick={this.onLogout.bind(this)}>
+                                    Log Out
+                                </Button>
                             </OverlayTrigger>
                         </Col>
                     </Row>
@@ -69,48 +69,69 @@ export class AccountSettings extends React.Component<{}, AccountSettingsState> {
                         <Col md={6} mdOffset={3}>
                             <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={popover}>
                                 <Button className="button-logout" onClick={this.onDeleteAllData.bind(this)}>
-                                  Delete Account
+                                    Delete Account
                                 </Button>
                             </OverlayTrigger>
                         </Col>
                     </Row>
                 </Well>
+                <Well>
+                    <Row>
+                        <Col md={12}>
+                            <div className="blacklist-info">
+                                Blacklist people here
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} mdOffset={3}>
+                            <Button className="blacklist_button" onClick={this.onBlacklistFill.bind(this)}>
+                                    Blacklist
+                            </Button>
+                        </Col>
+                    </Row >
+                </Well >
             </div>
         )
     }
 
-  onLogout(e: React.MouseEvent) {
-    e.stopPropagation();
-
-    if (this.state.logoutConfirmed) {
-      Meteor.logout();
+    onBlacklistFill(e: React.MouseEvent) {
+        console.log("Puzzi");
     }
 
-    this.setState({
-      logoutConfirmed: true,
-      deleteAllDataConfirmed: false
-    })
-  }
 
-  onDeleteAllData(e:React.MouseEvent) {
-    e.stopPropagation();
+    onLogout(e: React.MouseEvent) {
+        e.stopPropagation();
 
-    if (this.state.deleteAllDataConfirmed) {
-      Meteor.call("Account.deleteAllData");
+        if (this.state.logoutConfirmed) {
+            Meteor.logout();
+        }
+
+        this.setState({
+            logoutConfirmed: true,
+            deleteAllDataConfirmed: false
+        })
     }
 
-    this.setState({
-      logoutConfirmed: false,
-      deleteAllDataConfirmed: true
-    })
-  }
+    onDeleteAllData(e: React.MouseEvent) {
+        e.stopPropagation();
 
-  onBackgroundClick() {
-    this.setState({
-      logoutConfirmed: false,
-      deleteAllDataConfirmed: false
-    });
-  }
+        if (this.state.deleteAllDataConfirmed) {
+            Meteor.call("Account.deleteAllData");
+        }
+
+        this.setState({
+            logoutConfirmed: false,
+            deleteAllDataConfirmed: true
+        })
+    }
+
+    onBackgroundClick() {
+        this.setState({
+            logoutConfirmed: false,
+            deleteAllDataConfirmed: false
+        });
+    }
 
 }
 
