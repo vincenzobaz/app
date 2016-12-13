8bdb7863a411844cf1e387f61abdaec21e72b624
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2016-12-13 12:01:41 +0100

    Change the graph into a StackedArea chart displaying the proprtions of tie, won and lost games by date

--- meteor/imports/client/components/stats/PlayedTimeLine.tsx
+++ meteor/imports/client/components/stats/PlayedTimeLine.tsx
@@ -1,28 +1,40 @@
-
 import {Stats} from "../../../common/models/Stats";
 const LineChart = require('react-chartjs-2').Line;
 
 export function PlayTimeLineChart(props) {
     const stats: Stats[] = props.data;
-    let won: number[] = [];
-    let played: number[] = [];
-    let dates: string[] = [];
+    /*
+     Chartjs does not provide a stacked area chart, therefore
+     we have to simulate it using LineGraph with area under them.
+     Therefore we compute the percentage of the value we want to display at the bottom and we add it
+     to the value we want to display just above.
+     */
 
-    stats.forEach(stat => {
-        won.push(stat.win);
-        played.push(stat.amount);
-        dates.push(stat.date.toLocaleDateString())
+    // Arrays containing added percentages.
+    let won: number[] = [];
+    let lost: number[] = [];
+    let tie: number[] = [];
+
+    stats.forEach((s, i) => {
+        won.push((s.win * 100 / s.amount));
+        lost.push((s.loss * 100 / s.amount) + won[i]);
+        tie.push((s.tie * 100 / s.amount) + lost[i]);
     });
 
     let data = {
-        labels: dates,
+        labels: stats.map((s) => s.date.toLocaleDateString()),
         datasets: [{
             data: won,
-            label: 'Games won by date',
-            backgroundColor: 'rgba(0, 200, 0, 0.8)'
+            label: 'Games won by date (%)',
+            backgroundColor: 'rgba(0, 200, 0, 1)'
         }, {
-            data: played,
-            label: 'Games played by date'
+            data: lost,
+            label: 'Games lost by date (%)',
+            backgroundColor: 'rgba(200, 0, 0, 1)'
+        }, {
+            data: tie,
+            label: 'Tie by date (%)',
+            backgroundColor: 'rgba(255, 190, 0, 1)'
         }]
     };
 
